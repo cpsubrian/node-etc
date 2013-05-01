d0805b42ecf40d299d4373bc4f1bc64e929c8565
Brian Link <cpsubrian@gmail.com>
2013-05-01 14:16:19 -0700

    Code style in tests.

--- test/conf.js
+++ test/conf.js
@@ -1,43 +1,43 @@
 var etc = require('../etc'),
     assert = require('assert');
 
-describe('Configuration methods', function() {
+describe('Configuration methods', function () {
   var conf;
 
-  beforeEach(function() {
+  beforeEach(function () {
     conf = etc();
   });
 
-  it('can add basic conf from objects', function() {
+  it('can add basic conf from objects', function () {
     var test = {test: 'object'};
     conf.add(test);
     assert.equal(conf.get('test'), 'object', 'The configuration was not added');
     assert.deepEqual(conf.toJSON(), test);
   });
 
-  it('can get nested conf from objects', function() {
+  it('can get nested conf from objects', function () {
     conf.add({one: {two: {three: 'test'}}});
     assert.equal(conf.get('one:two:three'), 'test');
   });
 
-  it('can set basic conf', function() {
+  it('can set basic conf', function () {
     conf.add({name: 'Brian'});
     conf.set('name', 'Carlos');
     assert.equal(conf.get('name'), 'Carlos');
   });
 
-  it('can set nested conf', function() {
+  it('can set nested conf', function () {
     conf.add({player: {name: 'Brian'}});
     conf.set('player:name', 'Carlos');
     assert.equal(conf.get('player:name'), 'Carlos');
   });
 
-  it('can add conf from package.json', function() {
+  it('can add conf from package.json', function () {
     conf.pkg();
     assert.deepEqual(conf.get('meat'), {white: {chicken: "free-range"}});
   });
 
-  it('can read conf from ./etc', function() {
+  it('can read conf from ./etc', function () {
     conf.etc();
     assert.deepEqual(conf.get('candy'), {chewy: 'gum'});
     assert.deepEqual(conf.get('fruit'), {green: {apple: 'granny'}});
@@ -57,17 +57,17 @@ describe('Configuration methods', function() {
     delete process.env['test_user:handle'];
   });
 
-  it('can add conf using the `all` alias', function() {
+  it('can add conf using the `all` alias', function () {
     conf.all();
     assert.deepEqual(conf.get('fruit'), {green: {apple: 'granny'}, red: {apple: 'fuji'}});
   });
 
-  it('made-up key returns undefined', function() {
+  it('made-up key returns undefined', function () {
     conf.set('foo', 'bar');
     assert.strictEqual(conf.get('blah'), undefined);
   });
 
-  it('multi-part made-up key returns undefined', function() {
+  it('multi-part made-up key returns undefined', function () {
     conf.set('foo', {bar: true});
     assert.strictEqual(conf.get('baz:lol'), undefined);
   });
